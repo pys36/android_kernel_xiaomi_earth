deb5581bffedc43ff5b9b5faede04ff85eba6ea8
Pang YS <devbx@proton.me>
2025-01-12 22:07:27 +0800

    run.sh: add -O3 flag

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@ export KBUILD_BUILD_HOST="archlinux"
 export KBUILD_BUILD_USER="pangys"
 export TZ="Asia/Kuching"
 
-make -j$(nproc --all) O=out ARCH=arm64 earth_defconfig
-make -j$(nproc --all) ARCH=arm64 O=out \
+make -j$(nproc --all) CFLAGS=-O3 O=out ARCH=arm64 earth_defconfig
+make -j$(nproc --all) CFLAGS=-O3 ARCH=arm64 O=out \
                       HOSTCC="ccache clang" \
                       HOSTCXX="ccache clang++" \
                       CC="ccache clang" \
